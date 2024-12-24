930bcb4b997a070c2cf8006f4bc4041cb3107c07
Cypher2801 <147578076+Cypher2801@users.noreply.github.com>
2024-12-24 20:22:21 +0530

    User and video models completed

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-// import dotenv from "dotenv"
-// dotenv.config()
-
+import app from "../app.js";
 const connectDb = async () => {
     try {
         await mongoose.connect(`${process.env.MONGO_DB_URL}/${DB_NAME}`);
+        app.on("error" , (error) => {
+            console.log("Mongo Connection Error :: Error :: " , error)
+        })
         console.log("Connected to DB");
     } catch (error) {
         console.log("Mongo Connection Error :: Error :: " , error)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import dotenv from 'dotenv'
 import connectDb from "./db/index.js";
-import express from "express";
+import app from './app.js';
 
 dotenv.config({
     path: './env'
 })
-const app = express()
-app.listen(process.env.PORT , () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+connectDb()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`);
+    })
 })
-connectDb();
+.catch((error) => {
+    console.log("Mongo Connection Error :: Error :: " , error);
+})
